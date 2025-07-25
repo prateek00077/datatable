b12192d1fa35a924966a6246fbea6f7060e0b0ab
prateek00077 <pandeyprateek560@gmail.com>
2025-07-25 19:47:35 +0530

    centered everything

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Table from "./components/Table"
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold underline">Data table</h1>
       <Table/>
     </div>
